19c5818017c44f3cf2a947db8c7a8cf43f293619
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-12-26 18:45:34 +0100

    Implement font size in the demo and also fix a bug on blurring for large font size

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   const [dummyText, setDummyText] = useState('Dummy text');
   const [text, setText] = useState('');
   const [textAfter, setTextAfter] = useState(4);
+  const [fontSize, setFontSize] = useState(20);
   const [skeletonType, setSkeletonType] = useState<'blurred' | 'gradient'>('blurred');
 
   useEffect(() => {
@@ -16,7 +17,8 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h3>Configure</h3>
+      <hr />
+      <h2>Configure</h2>
       <div>
         <label htmlFor="dummy-text">Dummy text: </label>
         <input id="dummy-text" type="text" value={dummyText} onChange={e => setDummyText(e.target.value)} />
@@ -30,6 +32,18 @@ const App: React.FC = () => {
           max={10}
           value={textAfter}
           onChange={e => setTextAfter(parseInt(e.target.value))}
+        />
+        second(s)
+      </div>
+      <div>
+        <label htmlFor="font-size">Font size: </label>
+        <input
+          type="number"
+          name="font-size"
+          min={1}
+          max={40}
+          value={fontSize}
+          onChange={e => setFontSize(parseInt(e.target.value))}
         />
       </div>
       <div>
@@ -58,11 +72,13 @@ const App: React.FC = () => {
         </label>
       </div>
       <hr />
-      <h2>Products demo: </h2>
+      <h2>Product demo: </h2>
       <hr />
-      <TestPlugin dummyText={dummyText} skeletonType={skeletonType}>
-        {text}
-      </TestPlugin>
+      <span style={{ fontSize }}>
+        <TestPlugin dummyText={dummyText} skeletonType={skeletonType}>
+          {text}
+        </TestPlugin>
+      </span>
     </div>
   );
 };
